3fbd65a3af42c184962e3dc10144ce148b40cd95
Benjamin <benjamind10@pm.me>
2022-05-30 14:43:45 -0400

    fixed spacing on hamburger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,6 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', mobileMenu);
-
 function mobileMenu() {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+  $('.hamburger').toggleClass('active');
+  $('.nav-menu').toggleClass('active');
 }
+
+$('.hamburger').click(mobileMenu);
